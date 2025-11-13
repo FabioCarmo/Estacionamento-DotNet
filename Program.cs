70860d4e2514f580469e561fd083d3189c6d6032
fabio <fcarmouk13@gmail.com>
2025-11-13 15:10:28 -0300

    Feat: Adicionado chamada dos metodos. O que importa e rodar..

--- Program.cs
+++ Program.cs
@@ -1,22 +1,24 @@
 ﻿
 using DesafioEstacionamento.Models;
 
+// algumas variaveis uteis
 decimal preco_hora = 0;
 decimal preco_inicial = 0;
 bool programa = true;
 int opcao;
 string placa;
+double tempo_estacionado;
 
+// Bem-vindo se voce estiver de boa
 Console.WriteLine("Bem-vindo ao sistema de estacionamento!");
-do {
-    Console.WriteLine("Digite o Preco de Entrada para Estacionar: ");
-    preco_inicial = Convert.ToDecimal(Console.ReadLine());
-    Console.WriteLine("Agora Digite o Preco Hora para Estacionar: ");
-    preco_hora = Convert.ToDecimal(Console.ReadLine());
-} while (preco_inicial <= 0 && preco_hora <= 0);
+Console.WriteLine("Digite o Preco de Entrada para Estacionar: ");
+preco_inicial = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Agora Digite o Preco Hora para Estacionar: ");
+preco_hora = Convert.ToDecimal(Console.ReadLine());
 
 Estacionamento est = new Estacionamento(preco_inicial, preco_hora); // Instancia o objeto
 
+// Mantem esse sisteminha em execucao
 while (programa) {
     Console.WriteLine(est.ExibirMenu());
     opcao = Convert.ToInt32(Console.ReadLine());
@@ -29,15 +31,17 @@ while (programa) {
         case 1:
             Console.WriteLine("Digite a placa do veiculo: ");
             placa = Console.ReadLine();
-            est.CadastrarVeiculo(placa);
+            Console.WriteLine(est.CadastrarVeiculo(placa));
             break;
         case 2:
             Console.WriteLine("Digite a placa do veiculo: ");
             placa = Console.ReadLine();
-            est.RemoverVeiculo(placa);
+            Console.WriteLine("Digite o tempo em que o veiculo ficou estacionado: ");
+            tempo_estacionado = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(est.RemoverVeiculo(placa, tempo_estacionado));
             break;
         case 3:
-            est.ListarVeiculo();
+            Console.WriteLine(est.ListarVeiculo());
             break;
         default:
             Console.WriteLine("Opcao Invalida !");
@@ -45,12 +49,14 @@ while (programa) {
     }
 }
 
-// Inicio:
-// 1 - Preco Inicial
-// 2 - Preco Por Hora
+/* 
+Inicio:
+1 - Preco Inicial
+2 - Preco Por Hora
 
-// Menu:
-// 1 - Cadastrar veículo (Placa)
-// 2 - Remover veículo (Placa)
-// 3 - Listar veículos (Placa)
-// 4 - Sair
+Menu:
+1 - Cadastrar veículo (Placa)
+2 - Remover veículo (Placa)
+3 - Listar veículos ()
+4 - Sair
+*/
